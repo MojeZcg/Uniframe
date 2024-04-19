acad4e7428aaa9baa73434db892783c7b6c24c55
Moje <jsMonte31@gmail.com>
2024-04-19 19:14:23 -0300

    Fix: the link of the MobileSlider.tsx

--- src/component/MobileSlider.tsx
+++ src/component/MobileSlider.tsx
@@ -5,7 +5,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { ShoppingCartIcon } from "@heroicons/react/20/solid";
-import { Image } from "@nextui-org/react";
+import { Button, Image } from "@nextui-org/react";
+import Link from "next/link";
 
 interface Product {
   product_id: number;
@@ -13,13 +14,10 @@ interface Product {
   product_images: string;
   product_price: string;
   product_description: string;
+  product_available: boolean;
 }
 
-interface Props {
-  products: Product[];
-}
-
-const MobileSlider: React.FC<Props> = ({ products }) => {
+const MobileSlider = ({ products }: { products: Product[] }) => {
   const settings = {
     dots: false,
     infinite: true,
@@ -34,28 +32,46 @@ const MobileSlider: React.FC<Props> = ({ products }) => {
 
   return (
     <Slider {...settings} className="z-50">
-      {products.map((product) => (
-        <div
-          key={product.product_id}
-          className="min-w-[20rem] max-w-[20rem] overflow-hidden rounded-none bg-[#121212] px-3 py-4 "
+      {products.map((p) => (
+        <Link
+          key={p.product_id}
+          href={`/store/product/${p.product_id}`}
+          className="flex  h-[29rem] w-[19rem] flex-col items-start justify-center overflow-hidden rounded-xl  border bg-[#0a0a0a] p-3  "
         >
-          <div className="  flex w-full items-center justify-center overflow-hidden px-2 py-1">
+          <div className="  flex w-full items-center justify-center overflow-hidden px-2 py-2">
             <Image
-              src={product.product_images}
-              alt={product.product_name}
-              className=" h-64 rounded-sm "
+              src={p.product_images.split(",")[0]}
+              alt={p.product_name}
+              className=" h-64  rounded-sm "
             />
           </div>
-          <div className="flex flex-col">
-            <h6 className="block py-2 text-xl">{product.product_name}</h6>
-            <div className="flex flex-col gap-0 pb-4 ">
-              <strong className="text-2xl">${product.product_price}</strong>
+          <div className="flex w-full flex-col">
+            <h6 className="font block truncate py-2 text-2xl font-semibold">
+              {p.product_name}
+            </h6>
+            <div className="flex flex-col gap-0 pb-2 ">
+              <p className="mb-1 line-clamp-2 text-sm text-neutral-300">
+                {p.product_description}
+              </p>
+              <strong className="text-2xl">${p.product_price}</strong>
             </div>
-            <button className=" flex w-full items-center justify-center gap-1.5 rounded-md border border-transparent bg-white py-1 text-black transition-all duration-200 ease-in-out hover:border-white hover:bg-[#0f0f0f] hover:text-white ">
-              Agregar al carrito <ShoppingCartIcon className="h-4 w-4" />
-            </button>
+
+            {p.product_available ? (
+              <Button radius="sm" color="success" variant="ghost" className="">
+                Agregar al carrito <ShoppingCartIcon className="h-5 w-5" />
+              </Button>
+            ) : (
+              <Button
+                isDisabled={true}
+                radius="sm"
+                color="default"
+                variant="ghost"
+              >
+                Agotado
+              </Button>
+            )}
           </div>
-        </div>
+        </Link>
       ))}
     </Slider>
   );
