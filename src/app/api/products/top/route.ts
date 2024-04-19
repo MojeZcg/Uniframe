d75fa418396fcec5f94aadebb87f361ba60cfc7d
Moje <jsMonte31@gmail.com>
2024-04-19 18:52:27 -0300

    Add: the condicion for the query

--- src/app/api/products/top/route.ts
+++ src/app/api/products/top/route.ts
@@ -3,6 +3,9 @@ import db from "@/lib/db";
 
 export async function GET() {
   const topProducts = await db.products.findMany({
+    where: {
+      product_available: true,
+    },
     orderBy: {
       product_view_count: "desc",
     },
