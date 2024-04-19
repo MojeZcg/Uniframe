84e2cd3f494fd046108420058bbeb1003b971db5
Moje <jsMonte31@gmail.com>
2024-04-19 14:36:12 -0300

    Add: New Style to the main carroucel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Carousel from "@/component/Carousel";
-import { Image, Divider, Button } from "@nextui-org/react";
+import { Image, Divider, Button, Card, Skeleton } from "@nextui-org/react";
 import PrincipalImageSkeleton from "@/component/skeletons/PrincipalImageSkeleton";
 import { Suspense } from "react";
 import { ShoppingCartIcon } from "@heroicons/react/20/solid";
 import MobileSlider from "@/component/MobileSlider";
 import { fetchTopProducts } from "@/lib/data";
+import TopProductsSkeleton from "@/component/skeletons/TopProductSkeleton";
+import Link from "next/link";
+import ImagesOfProduct from "@/component/ImagesOfProduct";
 
 export default async function Home() {
   const topProducts = await fetchTopProducts();
@@ -25,105 +28,140 @@ export default async function Home() {
   return (
     <div>
       <main className=" text-white  ">
-        <div className="mb-14 flex h-[calc(100vh-8rem)] flex-col items-center justify-center md:h-[calc(100vh-4.8rem)] md:flex-row 2xl:h-[50rem] ">
-          <div className="z-30 flex h-[36rem] w-full flex-col items-center justify-center overflow-hidden bg-main-dark text-white md:h-[calc(100vh-4.8rem)] md:flex-row 2xl:h-[44rem]">
-            <div className="z-20 flex items-start justify-center border-b-1 border-white shadow-2xl  shadow-black md:h-[calc(100vh-4.8rem)] md:w-[33.5rem] md:bg-main-dark  2xl:h-[44rem] 2xl:w-[38rem]">
-              <div className="flex w-full flex-col items-center justify-center px-10  text-start">
-                <h3 className="pb-8 pt-10 text-5xl font-extrabold leading-[3.1rem] text-neutral-200">
+        <div className=" flex h-[calc(100vh)] flex-col items-center md:h-[calc(100vh-4.8rem)] md:flex-row md:justify-center 2xl:h-[calc(100vh-15.55rem)] ">
+          <div className="z-30 flex h-[calc(100vh)] w-full flex-col items-center justify-center overflow-hidden bg-main-dark text-white md:h-[calc(100vh-4.8rem)] md:flex-row 2xl:h-[calc(100vh-15.55rem)]">
+            <div className="z-20 flex h-[calc(100vh-30rem)] items-start justify-center border-b-1 border-white bg-main-dark  shadow-2xl shadow-black md:h-[calc(100vh-4.8rem)] md:w-[33.5rem]  2xl:h-[calc(100vh-15.55rem)] 2xl:w-[44rem]">
+              <div className=" flex w-full flex-col items-center justify-center bg-main-dark px-8 text-start md:w-72 md:p-0 2xl:w-96">
+                <h3 className=" py-8 text-5xl font-extrabold leading-[3.1rem] text-neutral-200 md:pb-8 md:pt-10 md:text-5xl 2xl:py-9 2xl:text-6xl 2xl:font-black 2xl:leading-[4.5rem]">
                   Bienvenido a Uniframe!
                 </h3>
-                <p className=" text-[1.9rem] font-medium leading-tight text-neutral-500 ">
+                <p className=" text-2xl font-medium leading-tight text-neutral-500 md:text-[1.9rem] 2xl:text-[2.9rem] 2xl:leading-[1.21] ">
                   Encuentra tu inspiración en cada obra. Explora nuestra
                   exclusiva colección de cuadros y transforma tu espacio con
                   arte excepcional.
                 </p>
               </div>
             </div>
-            <div className=" h-[calc(100vh-5rem)] w-full cursor-default select-none overflow-hidden border-b-1 border-white xl:h-[calc(100vh-4.8rem)] 2xl:h-[44rem]">
+            <div className=" w-full cursor-default select-none overflow-hidden border-b-1 border-white xl:h-[calc(100vh-4.8rem)] 2xl:h-[calc(100vh-15.55rem)]">
               <Carousel images={images} />
             </div>
           </div>
         </div>
 
         <div className="flex w-full flex-col items-center justify-center ">
-          <h3 className=" mb-4 px-10 text-center text-2xl font-medium text-[#bfbfbf] underline underline-offset-[6px] md:text-4xl 2xl:text-4xl">
+          <h3 className=" mb-4 mt-10 px-10 text-center text-2xl font-medium text-[#bfbfbf] underline underline-offset-[6px] md:text-4xl 2xl:mb-8 2xl:mt-16 2xl:text-5xl">
             NUESTROS <span className=" font-bold text-white">PRODUCTOS</span>{" "}
             MAS VENDIDOS
           </h3>
-          <div className="py-8">
+
+          <div className=" md:py-8">
             <div className="flex w-full items-center justify-center">
-              <div className="block h-[32rem] w-[20rem] overflow-hidden  py-8 md:hidden">
+              <div className="block h-[32rem] w-[20.5rem] overflow-hidden py-6 md:hidden">
                 <MobileSlider products={topProducts} />
               </div>
             </div>
 
-            <div className="hidden  flex-wrap items-center justify-center gap-6 md:flex 2xl:hidden ">
-              {topProducts.slice(0, productsToShow["md"]).map((p: any) => (
-                <div
-                  key={p.product_id}
-                  className="flex h-[30rem] w-[19rem] flex-col items-start justify-center overflow-hidden rounded-md border bg-[#0a0a0a] px-3  shadow-lg shadow-gray-700   "
-                >
-                  <div className="  flex w-full items-center justify-center overflow-hidden px-2 py-2">
-                    <Image
-                      src={p.product_images.split(",")[0]}
-                      alt={p.product_name}
-                      className=" h-64  rounded-sm "
-                    />
-                  </div>
-                  <div className="flex w-full flex-col">
-                    <h6 className="font block truncate py-2 text-2xl font-semibold">
-                      {p.product_name}
-                    </h6>
-                    <div className="flex flex-col gap-0 pb-1 ">
-                      <p className="mb-1 line-clamp-2 text-sm text-neutral-300">
-                        {p.product_description}
-                      </p>
-                      <strong className="text-2xl">${p.product_price}</strong>
+            <div className="hidden flex-wrap items-center justify-center gap-6 md:flex 2xl:hidden ">
+              <Suspense fallback={<TopProductsSkeleton />}>
+                {topProducts.slice(0, productsToShow["md"]).map((p: any) => (
+                  <Link
+                    key={p.product_id}
+                    href={`/store/product/${p.product_id}`}
+                    className="flex h-[29.2rem] w-[19rem] flex-col items-start justify-center overflow-hidden rounded-xl  border bg-[#0a0a0a] px-3 shadow-lg shadow-gray-700   "
+                  >
+                    <div>
+                      <ImagesOfProduct
+                        name={p.product_name}
+                        images={p.product_images}
+                      />
                     </div>
-                    <Button radius="sm" color="success" variant="ghost">
-                      Agregar al carrito{" "}
-                      <ShoppingCartIcon className="h-4 w-4" />
-                    </Button>
-                  </div>
-                </div>
-              ))}
+                    <div className="flex w-full flex-col">
+                      <h6 className="font block truncate py-2 text-2xl font-semibold">
+                        {p.product_name}
+                      </h6>
+                      <div className="flex flex-col gap-0 pb-2 ">
+                        <p className="mb-1 line-clamp-2 text-sm text-neutral-300">
+                          {p.product_description}
+                        </p>
+                        <strong className="text-2xl">${p.product_price}</strong>
+                      </div>
+
+                      {p.product_available ? (
+                        <Button radius="sm" color="success" variant="ghost">
+                          Agregar al carrito{" "}
+                          <ShoppingCartIcon className="h-4 w-4" />
+                        </Button>
+                      ) : (
+                        <Button
+                          isDisabled
+                          radius="sm"
+                          color="default"
+                          variant="ghost"
+                        >
+                          Agotado
+                        </Button>
+                      )}
+                    </div>
+                  </Link>
+                ))}
+              </Suspense>
             </div>
 
-            <div className="hidden w-full flex-wrap items-center justify-center gap-6 2xl:flex ">
-              {topProducts.slice(0, productsToShow["lg"]).map((p: any) => (
-                <div
-                  key={p.product_id}
-                  className=" flex h-[36rem] w-[22rem] flex-col items-start justify-center overflow-hidden rounded-md border-2 bg-[#090909] px-3 py-4 shadow-xl shadow-gray-700"
-                >
-                  <div className="  flex w-full items-center justify-center overflow-hidden py-1">
-                    <Image
-                      src={p.product_images.split(",")[0]}
-                      alt={p.product_name}
-                      className=" h-72 rounded-sm "
-                    />
-                  </div>
-                  <div className="flex w-full flex-col">
-                    <h6 className="block truncate py-3 text-3xl">
-                      {p.product_name}
-                    </h6>
-                    <div className="flex flex-col gap-0 pb-6 ">
-                      <p className=" mb-2 line-clamp-2 text-base text-neutral-300">
-                        {p.product_description}
-                      </p>
-                      <strong className="text-3xl">${p.product_price}</strong>
+            <div className="hidden w-full flex-wrap items-center justify-center gap-7 2xl:flex ">
+              <Suspense fallback={<TopProductsSkeleton />}>
+                {topProducts.slice(0, productsToShow["lg"]).map((p: any) => (
+                  <Link
+                    key={p.product_id}
+                    href={`/store/product/${p.product_id}`}
+                    className="flex h-[35.5rem] w-[22rem] flex-col items-start justify-center overflow-hidden rounded-xl  border bg-[#0a0a0a] px-3 shadow-lg shadow-gray-700   "
+                  >
+                    <div className="  flex w-full items-center justify-center overflow-hidden px-2 py-2">
+                      <Image
+                        src={p.product_images.split(",")[0]}
+                        alt={p.product_name}
+                        className=" h-72  rounded-sm "
+                      />
                     </div>
-                    <button className=" flex w-full items-center justify-center gap-1.5 rounded-sm border border-transparent bg-white py-2 text-2xl text-black transition-all duration-200 ease-in-out hover:border-white hover:bg-black hover:text-white">
-                      Agregar al carrito{" "}
-                      <ShoppingCartIcon className="h-6 w-6" />
-                    </button>
-                  </div>
-                </div>
-              ))}
+                    <div className="flex w-full flex-col">
+                      <h6 className="font block truncate py-2 text-3xl font-semibold">
+                        {p.product_name}
+                      </h6>
+                      <div className="flex flex-col gap-0 pb-4 ">
+                        <p className="mb-3 line-clamp-2 text-xl text-neutral-300">
+                          {p.product_description}
+                        </p>
+                        <strong className="text-3xl">${p.product_price}</strong>
+                      </div>
+
+                      {p.product_available ? (
+                        <Button
+                          radius="sm"
+                          color="success"
+                          variant="ghost"
+                          className="h-14 text-2xl"
+                        >
+                          Agregar al carrito{" "}
+                          <ShoppingCartIcon className="h-6 w-6" />
+                        </Button>
+                      ) : (
+                        <Button
+                          isDisabled
+                          radius="sm"
+                          color="default"
+                          variant="ghost"
+                        >
+                          Agotado
+                        </Button>
+                      )}
+                    </div>
+                  </Link>
+                ))}
+              </Suspense>
             </div>
           </div>
         </div>
       </main>
-      <h2 className="mb-4 pl-5 pt-10 text-4xl font-normal text-white">
+      <h2 className="mb-4 pl-5 text-4xl font-normal text-white md:pt-10">
         PROCESO DE ELABORACIÓN
       </h2>
       <div className="flex w-full items-center justify-center  ">
