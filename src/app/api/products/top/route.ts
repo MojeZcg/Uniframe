edb875e943eb194b7ff65928b5d934b23aa6be3c
Moje <jsMonte31@gmail.com>
2024-04-26 20:22:19 -0300

    Add: the number of products availables

--- src/app/api/products/top/route.ts
+++ src/app/api/products/top/route.ts
@@ -3,9 +3,6 @@ import db from "@/lib/db";
 
 export async function GET() {
   const topProducts = await db.products.findMany({
-    where: {
-      product_available: true,
-    },
     orderBy: {
       product_view_count: "desc",
     },
