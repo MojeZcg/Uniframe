3e48778e2a1460336912699e0670f675885fc5f0
Moje <jsMonte31@gmail.com>
2024-04-29 16:57:59 -0300

    Add: global types for typescript

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export type Product = {
+  product_id: number;
+  product_name: string;
+  product_price: string;
+  product_images: string;
+  product_availables: number;
+  product_description: string;
+  product_view_count: number;
+  product_created_at: string;
+};
