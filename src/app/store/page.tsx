053a29e5b43dd9fd9bd99a144a411b4a9deedeeb
Moje <jsMonte31@gmail.com>
2024-05-21 16:49:33 -0300

    Fix: add search input and fix the accordion

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -21,6 +21,9 @@ import { useEffect, useState } from "react";
 import { Product } from "../../lib/types";
 import ProductCard from "@/components/layout/ProductCard";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
+import Search from "@/components/layout/Search";
+import { FiPackage } from "react-icons/fi";
 
 export default function Store() {
   const [products, setProducts] = useState<Product[] | null>(null);
@@ -52,74 +55,95 @@ export default function Store() {
   ];
 
   return (
-    <div className=" bg-main-dark flex h-auto w-full justify-between  pt-6 ">
-      <div className="ml-3 w-[18rem] 2xl:w-[30rem]">
-        <h3 className=" text-2xl ">Filtros</h3>
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Precio</AccordionTrigger>
-            <AccordionContent>
-              Yes. It adheres to the WAI-ARIA design pattern.
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-2">
-            <AccordionTrigger>Materiales</AccordionTrigger>
-            <AccordionContent className="flex flex-col">
-              {materials.map((material) => (
-                <div
-                  key={material.id}
-                  className="flex items-center gap-2 py-1.5"
-                >
-                  <Checkbox id={material.slug} className="h-5 w-5 rounded-lg" />
-                  <Label htmlFor={material.slug}>{material.name}</Label>
-                </div>
-              ))}
-            </AccordionContent>
-          </AccordionItem>
-          <AccordionItem value="item-3">
-            <AccordionTrigger>Envio</AccordionTrigger>
-            <AccordionContent>
-              <div className="flex items-center gap-2 ">
-                <Switch id="airplane-mode" />
-                <Label htmlFor="airplane-mode">Airplane Mode</Label>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-      <div className=" w-[calc(100vw-18rem)]">
-        <div className=" bg-main-dark flex flex-wrap items-center justify-center gap-x-4 gap-y-3  ">
-          {loading && <ProductsSkeleton timesSkeleton={9} />}
-          {products?.map((p: Product) => (
-            <ProductCard key={p.product_id} product={p} />
-          ))}
+    <div>
+      <div className="">
+        <div className="flex items-center justify-between pt-4">
+          <h1 className="pl-6 text-3xl font-semibold">MARCOS</h1>
+          <div className="mr-4 w-[48%] text-black">
+            <Search placeholder="" />
+          </div>
         </div>
-        <div className="mt-10 flex w-full items-center justify-end pr-10">
-          <div className=" pr-5 ">
-            <Pagination>
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious href="#" />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#" isActive className="text-black">
-                    2
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href="#">3</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationNext href="#" />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
+
+        <Separator className=" mb-5 mt-4 h-0.5 w-full bg-white" />
+      </div>
+
+      <div className=" flex h-auto w-full justify-between  ">
+        <div className="ml-3 w-[18rem] 2xl:w-[30rem]">
+          <h3 className=" text-2xl ">Filtros</h3>
+          <Accordion type="single" className="w-full">
+            <AccordionItem value="price">
+              <AccordionTrigger>Precio</AccordionTrigger>
+              <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="meterials">
+              <AccordionTrigger>Materiales</AccordionTrigger>
+              <AccordionContent className="flex flex-col">
+                {materials.map((material) => (
+                  <div
+                    key={material.id}
+                    className="flex items-center gap-2 py-1.5"
+                  >
+                    <Checkbox
+                      id={material.slug}
+                      className="h-5 w-5 rounded-lg"
+                    />
+                    <Label htmlFor={material.slug}>{material.name}</Label>
+                  </div>
+                ))}
+              </AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger className="text-lg">Envio</AccordionTrigger>
+              <AccordionContent>
+                <div className="flex items-center gap-1.5 py-1">
+                  <div className="flex items-center gap-2">
+                    <Switch id="airplane-mode" />
+                    <Label htmlFor="airplane-mode" className="">
+                      Envio Gratis
+                    </Label>
+                  </div>
+                  <FiPackage className="h-5 w-5 text-green-600" />
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        </div>
+        <div className=" w-[calc(100vw-18rem)]">
+          <div className=" bg-main-dark flex flex-wrap items-center justify-center gap-x-4 gap-y-3  ">
+            {loading && <ProductsSkeleton timesSkeleton={9} />}
+            {products?.map((p: Product) => (
+              <ProductCard key={p.product_id} product={p} />
+            ))}
+          </div>
+          <div className="mt-10 flex w-full items-center justify-end pr-10">
+            <div className=" pr-5 ">
+              <Pagination>
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious href="#" />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#" isActive className="text-black">
+                      2
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationEllipsis />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationNext href="#" />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+            </div>
           </div>
         </div>
       </div>
