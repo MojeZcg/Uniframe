2f8b8682bcf9123021b870059fd3c0570367c26b
Moje <jsMonte31@gmail.com>
2024-05-22 19:31:31 -0300

    feat: inprove the ProductSkeleton

--- src/components/skeletons/ProductsSkeleton.tsx
+++ src/components/skeletons/ProductsSkeleton.tsx
@@ -6,12 +6,20 @@ export default function ProductsSkeleton({
   return Array.from({ length: timesSkeleton }, (_, i) => (
     <div
       key={i}
-      className="flex flex-col space-y-3 rounded-md bg-neutral-700 p-2 "
+      className="flex h-[30rem] w-[20rem] flex-col items-center space-y-3 rounded-xl border border-neutral-600 bg-neutral-950 p-2 "
     >
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
-        <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
+      <Skeleton className="my-4 h-64 w-52 rounded-xl" />
+      <div className="space-y-3.5">
+        <div className="space-y-3">
+          <Skeleton className="h-6 w-72" />
+          <div className="space-y-1">
+            <Skeleton className="h-3 w-72" />
+            <Skeleton className="h-3 w-[200px]" />
+          </div>
+          <Skeleton className="h-7 w-28" />
+        </div>
+
+        <Skeleton className="h-10 w-72 rounded-md" />
       </div>
     </div>
   ));
