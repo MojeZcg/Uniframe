6169bd033500acd1798b419e2c61c90246398442
Moje <jsMonte31@gmail.com>
2024-05-30 11:31:43 -0300

    fix: position of the image in the skeleton

--- src/components/skeletons/ProductsSkeleton.tsx
+++ src/components/skeletons/ProductsSkeleton.tsx
@@ -6,10 +6,10 @@ export default function ProductsSkeleton({
   return Array.from({ length: timesSkeleton }, (_, i) => (
     <div
       key={i}
-      className="flex h-[30rem] w-[20rem] flex-col items-center space-y-3 rounded-xl border border-neutral-600 bg-neutral-950 p-2 "
+      className="flex h-[29rem] w-[20rem] flex-col items-center justify-center space-y-3 rounded-xl border border-neutral-600 bg-neutral-950 p-2 "
     >
-      <Skeleton className="my-4 h-64 w-52 rounded-xl" />
-      <div className="space-y-3.5">
+      <Skeleton className="my-2 h-64 w-52 rounded-xl" />
+      <div className="space-y-3 ">
         <div className="space-y-3">
           <Skeleton className="h-6 w-72" />
           <div className="space-y-1">
@@ -18,8 +18,7 @@ export default function ProductsSkeleton({
           </div>
           <Skeleton className="h-7 w-28" />
         </div>
-
-        <Skeleton className="h-10 w-72 rounded-md" />
+        <Skeleton className=" h-10 w-72 rounded-md" />
       </div>
     </div>
   ));
