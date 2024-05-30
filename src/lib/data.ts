aafbad6a9884480f47106ebe29d809227899eec5
Moje <jsMonte31@gmail.com>
2024-05-30 11:24:03 -0300

    remove: unused functions

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -25,20 +25,3 @@ export const Socials = [
     icon: FaInstagram,
   },
 ];
-
-export async function fetchProducts() {
-  const res = await fetch("http://localhost:3000/api/products/");
-  const data = await res.json();
-  return data;
-}
-
-export async function fetchTopProducts() {
-  const res = await fetch("http://localhost:3000/api/products/top/", {
-    method: "GET",
-    next: {
-      revalidate: 5000,
-    },
-  });
-  const data = await res.json();
-  return data;
-}
