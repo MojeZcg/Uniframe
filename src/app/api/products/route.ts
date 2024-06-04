63cb9028c38d7b04a25aade78d801f50c3e5edcc
Moje <jsMonte31@gmail.com>
2024-06-03 21:12:01 -0300

    fix: the price filter and remove an unuser request.method verification

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -31,115 +31,111 @@ export async function GET(request: NextRequest) {
   const pageSize = 12;
   const skip = (pageNumber - 1) * pageSize;
 
-  if (request.method === "GET") {
-    try {
-      let priceFilter = {};
-      if (price) {
-        const [minPrice, maxPrice] = price.split(",").map(Number);
-        if (!isNaN(minPrice) && !isNaN(maxPrice)) {
-          priceFilter = {
-            product_price: {
-              gte: minPrice,
-              lte: maxPrice,
-            },
-          };
-        }
+  try {
+    let priceFilter = {};
+    if (price) {
+      const [minPrice, maxPrice] = price.split(",").map(Number);
+      if (!isNaN(minPrice) && !isNaN(maxPrice)) {
+        priceFilter = {
+          product_price: {
+            gte: minPrice,
+            lte: maxPrice,
+          },
+        };
       }
-
-      const totalCount = await db.products.count({
-        where: {
-          AND: [
-            {
-              OR: [
-                {
-                  product_name: {
-                    contains: queryParam,
-                    mode: "insensitive",
-                  },
-                },
-                {
-                  product_description: {
-                    contains: queryParam,
-                    mode: "insensitive",
-                  },
-                },
-              ],
-            },
-            priceFilter,
-          ],
-        },
-      });
-
-      const min = await db.products.aggregate({
-        _min: {
-          product_price: true,
-        },
-      });
-
-      const max = await db.products.aggregate({
-        _max: {
-          product_price: true,
-        },
-      });
-
-      const products = await db.products.findMany({
-        orderBy: order
-          ? {
-              product_price: order === "asc" ? "asc" : "desc",
-            }
-          : {
-              product_availables: "desc",
-            },
-        where: {
-          AND: [
-            {
-              OR: [
-                {
-                  product_name: {
-                    contains: queryParam,
-                    mode: "insensitive",
-                  },
-                },
-                {
-                  product_description: {
-                    contains: queryParam,
-                    mode: "insensitive",
-                  },
-                },
-              ],
-            },
-            priceFilter,
-          ],
-        },
-        skip: skip,
-        take: pageSize,
-      });
-
-      const totalPages = Math.ceil(totalCount / pageSize);
-
-      return NextResponse.json({
-        products,
-        min,
-        max,
-        pagination: {
-          totalCount,
-          totalPages,
-          currentPage: pageNumber,
-          pageSize,
-        },
-      });
-    } catch (error) {
-      console.error("Database query failed", error);
-
-      return NextResponse.json(
-        {
-          error:
-            "An error occurred while fetching products. Please try again later.",
-        },
-        { status: 500 },
-      );
     }
-  }
 
-  return NextResponse.json({ error: "Method not allowed" }, { status: 405 });
+    const totalCount = await db.products.count({
+      where: {
+        AND: [
+          {
+            OR: [
+              {
+                product_name: {
+                  contains: queryParam,
+                  mode: "insensitive",
+                },
+              },
+              {
+                product_description: {
+                  contains: queryParam,
+                  mode: "insensitive",
+                },
+              },
+            ],
+          },
+          priceFilter,
+        ],
+      },
+    });
+
+    const min = await db.products.aggregate({
+      _min: {
+        product_price: true,
+      },
+    });
+
+    const max = await db.products.aggregate({
+      _max: {
+        product_price: true,
+      },
+    });
+
+    const products = await db.products.findMany({
+      orderBy: order
+        ? {
+            product_price: order === "asc" ? "asc" : "desc",
+          }
+        : {
+            product_availables: "desc",
+          },
+      where: {
+        AND: [
+          {
+            OR: [
+              {
+                product_name: {
+                  contains: queryParam,
+                  mode: "insensitive",
+                },
+              },
+              {
+                product_description: {
+                  contains: queryParam,
+                  mode: "insensitive",
+                },
+              },
+            ],
+          },
+          priceFilter,
+        ],
+      },
+      skip: skip,
+      take: pageSize,
+    });
+
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    return NextResponse.json({
+      products,
+      min,
+      max,
+      pagination: {
+        totalCount,
+        totalPages,
+        currentPage: pageNumber,
+        pageSize,
+      },
+    });
+  } catch (error) {
+    console.error("Database query failed", error);
+
+    return NextResponse.json(
+      {
+        error:
+          "An error occurred while fetching products. Please try again later.",
+      },
+      { status: 500 },
+    );
+  }
 }
