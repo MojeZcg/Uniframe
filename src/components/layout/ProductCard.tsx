f290aca904b75f29c439eb282584bc3839303de4
Moje <jsMonte31@gmail.com>
2024-06-05 19:29:51 -0300

    feat: new style of the store page

--- src/components/layout/ProductCard.tsx
+++ src/components/layout/ProductCard.tsx
@@ -7,7 +7,6 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
 import { Card } from "@/components/ui/card";
 import Link from "next/link";
 import { ShoppingCartIcon } from "@heroicons/react/16/solid";
@@ -19,25 +18,25 @@ export default function ProductCard({
   const split = product.product_images.split(",");
   const images = split.map((url, index) => {
     return {
-      id: index + 1, // Asigna el índice incrementado en 1 como id
+      id: index + 1,
       src: url,
     };
   });
   return (
     <Card
       key={product.product_id}
-      className=" flex h-[29rem] max-h-[29rem] max-w-[20rem] flex-col items-start justify-center overflow-hidden rounded-xl border border-neutral-400 bg-transparent px-4 pb-2 text-white  shadow-md shadow-gray-700 "
+      className="flex h-[29rem] max-h-[29rem] max-w-[20rem] flex-col items-start justify-center overflow-hidden rounded-xl border border-neutral-400 bg-neutral-950 p-4 text-white"
     >
       <div className="z-50 ml-auto mr-auto flex h-[16.5rem] w-[13rem] items-center justify-center pt-1">
         <Carousel>
           <CarouselContent>
             {images.map((image) => (
               <CarouselItem key={image.id}>
-                <div className="flex  h-64 select-none items-center justify-center ">
+                <div className="flex h-64 select-none items-center justify-center">
                   <Image
                     src={image.src}
-                    alt={product.product_name + " Image - " + image.id}
-                    className=" object-contain"
+                    alt={`${product.product_name} Image - ${image.id}`}
+                    className="object-contain"
                     width={1200}
                     height={1400}
                   />
@@ -55,22 +54,31 @@ export default function ProductCard({
         className="h-auto w-full"
       >
         <div className="flex w-full flex-col">
-          <h6 className="font block truncate py-2 text-2xl font-semibold">
+          <h6 className="block truncate py-2 text-2xl font-semibold text-gray-200">
             {product.product_name}
           </h6>
-          <div className="flex flex-col gap-0 pb-2 ">
-            <p className="mb-1 line-clamp-2 text-sm">
+          <div className="flex flex-col gap-0 pb-2">
+            <p className="mb-1 line-clamp-2 text-sm text-gray-400">
               {product.product_description}
             </p>
-            <strong className="text-2xl">${product.product_price}</strong>
+            <strong className="text-2xl text-gray-300">
+              ${product.product_price}
+            </strong>
           </div>
 
           {product.product_availables != 0 ? (
-            <Button variant="outline" className="rounded-lg ">
+            <Button
+              variant="outline"
+              className="rounded-lg border-gray-500 text-gray-300 hover:bg-gray-700"
+            >
               Agregar al carrito <ShoppingCartIcon className="ml-2 h-5 w-5" />
             </Button>
           ) : (
-            <Button disabled variant="outline">
+            <Button
+              disabled
+              variant="outline"
+              className="border-gray-600 text-gray-400"
+            >
               Sold Out
             </Button>
           )}
