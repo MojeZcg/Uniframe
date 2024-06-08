8e7bd71c2a075317e2caea5009d5fc59945102a8
Moje <jsMonte31@gmail.com>
2024-06-07 23:07:41 -0300

    style: add the avatar, name and email of the account

--- src/components/layout/LayoutSearch.tsx
+++ src/components/layout/LayoutSearch.tsx
@@ -8,11 +8,17 @@ export default function LayoutSearch() {
   return pathname != "/store" ? (
     <Search placeholder="Busca productos, materiales y más..." />
   ) : (
-    <div>
+    <div className="flex items-center gap-3 pr-4 text-black">
       <Avatar>
         <AvatarImage src="https://github.com/MojeZcg.png" />
-        <AvatarFallback>WM</AvatarFallback>
+        <AvatarFallback className="bg-neutral-300">WM</AvatarFallback>
       </Avatar>
+      <div>
+        <h3>Hola, Walter!</h3>
+        <h4 className="max-w-48 truncate text-xs">
+          walterfotodigital101@gmail.com
+        </h4>
+      </div>
     </div>
   );
 }
