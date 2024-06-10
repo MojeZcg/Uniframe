aa2e91ee1df832bdd5c887615f017aa388a90cd9
Moje <jsMonte31@gmail.com>
2024-06-09 23:51:49 -0300

    add: the fetch of the product page

--- src/app/store/product/[product_id]/page.tsx
+++ src/app/store/product/[product_id]/page.tsx
@@ -1,6 +1,32 @@
+"use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-export default function page({ params }: { params: { product_id: string } }) {
+export function productPage({ params }: { params: { product_id: string } }) {
+  const [loading, setLoading] = useState(false);
+  const [product, setProduct] = useState();
+
+  const getProducts = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/products/${params.product_id}/`,
+      );
+      if (res) {
+        const data = await res.json();
+        if (data) setProduct(data);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <section className="">
       <div> </div>
